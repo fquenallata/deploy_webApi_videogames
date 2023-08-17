e69c13d55fefb99cb1a86e4f4bdb90c6f704bbcc
Quena <quenallatafranco@gmail.com>
2023-08-17 18:31:48 -0300

    refactorizando api

--- api/src/app.js
+++ api/src/app.js
@@ -9,7 +9,10 @@ server.name = "API";
 server.use(express.json());
 
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://deploy-web-api-videogames.vercel.app"
+  ); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
