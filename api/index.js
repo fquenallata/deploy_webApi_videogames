df6623cb6d0b24b21976c1cf643722feb2a97fc4
Quena <quenallatafranco@gmail.com>
2023-08-17 21:34:20 -0300

    refactorizando

--- api/index.js
+++ api/index.js
@@ -1,11 +1,11 @@
 require("dotenv").config();
 const app = require("./src/app.js");
 const { conn } = require("./src/db.js");
-const PORT = process.env.PORT || 3001;
+const { PORT, DB_HOST } = process.env;
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
   app.listen(PORT, () => {
-    console.log(`server raised in port: http://localhost:${PORT}`); // eslint-disable-line no-console
+    console.log(`server raised in port: ${DB_HOST}:${PORT}`); // eslint-disable-line no-console
   });
 });
